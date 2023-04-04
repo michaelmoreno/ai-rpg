b22acf37c2dedb1c84140f93dc2df885a97d2017
Michael Moreno <michaelmoreno.dev@gmail.com>
2023-04-04 05:24:09 -0600

    :diamond_shape_with_a_dot_inside: `add` character SleepState

---
+++ src/model/characters/states/SleepState.ts
@@ -0,0 +1,23 @@
+import { State } from "../../../patterns/StateMachine"
+import { Character } from "../Character"
+import { GlobalTime } from "../../../engine/GlobalTime"
+import { IdleState } from "./IdleState"
+
+class SleepState extends State {
+    duration: number
+    protected elapsed: number = 0
+
+    constructor(duration: number = 1200) {
+        super()
+        this.duration = duration
+    }
+
+    execute(context: Character): void {
+        this.elapsed += GlobalTime.delta
+        if (this.elapsed >= this.duration) {
+            context.enter(new IdleState())
+        }
+    }
+}
+
+export { SleepState }
--- tests/character.test.ts
+++ tests/character.test.ts
@@ -1,7 +1,9 @@
 import { Character } from '../src/model/characters/Character';
 import { Inventory } from '../src/model/characters/Inventory';
 import { IdleState } from '../src/model/characters/states/IdleState';
+import { SleepState } from '../src/model/characters/states/SleepState';
 import { State, StateMachine } from '../src/patterns/StateMachine';
+import { GlobalTime } from '../src/engine/GlobalTime';
 
 class MockState extends State {
     constructor() {
@@ -12,9 +14,13 @@ class MockState extends State {
 
 describe('Character', () => {
     let character: Character;
+    let timeCaller: object
 
     beforeEach(() => {
         character = new Character(1, 'Test Character', 'A test character', 100, new Inventory(100));
+        timeCaller = {}
+        GlobalTime.setApprovedCaller(timeCaller)
+        GlobalTime.reset(timeCaller)
     })
 
     it('can transition', () => {
@@ -23,5 +29,18 @@ describe('Character', () => {
         character.enter(new MockState())
         // @ts-expect-error
         expect(character.state).toBeInstanceOf(MockState)
+    })
+    it('can sleep and wake up', () => {
+        // @ts-expect-error
+        expect(character.state).not.toBeInstanceOf(SleepState)
+        character.enter(new SleepState(10))
+        // @ts-expect-error
+        expect(character.state).toBeInstanceOf(SleepState)
+        jest.useFakeTimers()
+        jest.advanceTimersByTime(10000)
+        GlobalTime.tick(timeCaller)
+        character.update()
+        // @ts-expect-error
+        expect(character.state).toBeInstanceOf(IdleState)
     })
 })
